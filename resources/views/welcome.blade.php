055b336074b926522d71c02a1588d1b160c60ed0
Rob Cooper <sunscreem@gmail.com>
2017-04-22 21:06:28 +0100

    Added photo gallery

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,7 +65,7 @@
             <li class="active"><a href="#home">Home</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#events">Events</a></li>
-            <li><a href="#gallery">Gallery</a></li>
+            <li><a href="#photos">Gallery</a></li>
             <li><a href="#information">Information</a></li>
             <li><a href="#contact">Contact</a></li>
           </ul>
@@ -133,6 +133,184 @@
     </section>
     <!-- end:about -->
 
+
+    <!-- begin:story -->
+    <section id="events">
+        <div class="line"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>This Years Events</h2>
+                    <ul class="content">
+                        <li>
+                            <h3 class="content-avatar avatar-top">
+                                <div class="img-avatar" style="background-image: url('/images/betteridge.jpg');"></div>
+                            </h3>
+                            <ul>
+                                
+                                <li class="content-item content-photo">
+                                    <img src="http://placekitten.com/750/500" alt="Lorem Ipsum">   
+                                    <p class="caption">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua. </p>
+                                    <time datetime=""><i class="fa fa-calendar"></i> 1st June 2017 - Doors Open 7pm</time>
+                                </li>
+
+                                <li class="content-item content-photo">
+                                    <img src="http://placekitten.com/750/500" alt="Lorem Ipsum">   
+                                    <p class="caption">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua. </p>
+                                    <time datetime=""><i class="fa fa-calendar"></i> 1st June 2017 - Doors Open 7pm</time>
+                                </li>
+
+                                <li class="content-item content-photo">
+                                    <img src="http://placekitten.com/750/500" alt="Lorem Ipsum">   
+                                    <p class="caption">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua. </p>
+                                    <time datetime=""><i class="fa fa-calendar"></i> 1st June 2017 - Doors Open 7pm</time>
+                                </li>
+
+                                <li class="content-item content-photo">
+                                    <img src="http://placekitten.com/750/500" alt="Lorem Ipsum">   
+                                    <p class="caption">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua. </p>
+                                    <time datetime=""><i class="fa fa-calendar"></i> 1st June 2017 - Doors Open 7pm</time>
+                                </li>
+
+                                <li class="content-item content-photo">
+                                    <img src="http://placekitten.com/750/500" alt="Lorem Ipsum">   
+                                    <p class="caption">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua. </p>
+                                    <time datetime=""><i class="fa fa-calendar"></i> 1st June 2017 - Doors Open 7pm</time>
+                                </li>
+
+                                <li class="content-item content-photo">
+                                    <img src="http://placekitten.com/750/500" alt="Lorem Ipsum">   
+                                    <p class="caption">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua. </p>
+                                    <time datetime=""><i class="fa fa-calendar"></i> 1st June 2017 - Doors Open 7pm</time>
+                                </li>
+
+                                
+                            </ul>
+                        </li>
+
+                        <li class="content-end">
+                            <h3 class="content-avatar avatar-end">
+                                <div class="img-avatar" style="background-image: url('/images/skateraw-hall.jpg');"></div>
+                            </h3>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- end:story -->
+
+    <!-- begin:photos -->
+    <section id="photos">
+        <div class="line"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="/images/bottom.png" class="about-img img-responsive">
+                    <h2>Photo Gallery</h2>
+                    <p>Some pictures taken from our <a href="https://www.facebook.com/skaterawfair/">Facebook Page</a> from previous years events.</p>
+                </div>
+                <!-- break -->
+
+                <div class="col-md-8">
+                    <div id="wedding-photo" class="carousel slide" data-ride="carousel">
+                      <div class="carousel-inner">
+                        <div class="item">
+                          <div class="row">
+                            <div class="col-md-4 col-sm-4 col-xs-12 animated fadeInLeftBig">    
+                                <a href="/images/gallery/01.jpg" class="gallery-images">
+                                    <div class="photo-gallery">
+                                        <img src="/images/gallery-thumbs/01.jpg" class="img-responsive" alt="ditinggalrabi-wedding-photo-01">
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="col-md-4 col-sm-4 col-xs-12 animated fadeInDownBig">
+                                <a href="/images/gallery/02.jpg" class="gallery-images">
+                                    <div class="photo-gallery">
+                                        <img src="/images/gallery-thumbs/02.jpg" class="img-responsive" alt="ditinggalrabi-wedding-photo-02">
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="col-md-4 col-sm-4 col-xs-12 animated fadeInRightBig">
+                                <a href="/images/gallery/03.jpg" class="gallery-images">
+                                    <div class="photo-gallery">
+                                        <img src="/images/gallery-thumbs/03.jpg" class="img-responsive" alt="ditinggalrabi-wedding-photo-03">
+                                    </div>
+                                </a>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="item active">
+                          <div class="row">
+                            <div class="col-md-4 col-sm-4 col-xs-12 animated fadeInRightBig">   
+                                <a href="/images/gallery/04.jpg" class="gallery-images">
+                                    <div class="photo-gallery">
+                                        <img src="/images/gallery-thumbs/04.jpg" class="img-responsive" alt="ditinggalrabi-wedding-photo-04">
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="col-md-4 col-sm-4 col-xs-12 animated fadeInUpBig">
+                                <a href="/images/gallery/05.jpg" class="gallery-images">
+                                    <div class="photo-gallery">
+                                        <img src="/images/gallery-thumbs/05.jpg" class="img-responsive" alt="ditinggalrabi-wedding-photo-05">
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="col-md-4 col-sm-4 col-xs-12 animated fadeInLeftBig">
+                                <a href="/images/gallery/06.jpg" class="gallery-images">
+                                    <div class="photo-gallery">
+                                        <img src="/images/gallery-thumbs/06.jpg" class="img-responsive" alt="ditinggalrabi-wedding-photo-06">
+                                    </div>
+                                </a>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="item">
+                          <div class="row">
+                            <div class="col-md-4 col-sm-4 col-xs-12 animated fadeInLeftBig">    
+                                <a href="/images/gallery/07.jpg" class="gallery-images">
+                                    <div class="photo-gallery">
+                                        <img src="/images/gallery-thumbs/07.jpg" class="img-responsive" alt="ditinggalrabi-wedding-photo-01">
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="col-md-4 col-sm-4 col-xs-12 animated fadeInDownBig">
+                                <a href="/images/gallery/08.jpg" class="gallery-images">
+                                    <div class="photo-gallery">
+                                        <img src="/images/gallery-thumbs/08.jpg" class="img-responsive" alt="ditinggalrabi-wedding-photo-02">
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="col-md-4 col-sm-4 col-xs-12 animated fadeInRightBig">
+                                <a href="/images/gallery/09.jpg" class="gallery-images">
+                                    <div class="photo-gallery">
+                                        <img src="/images/gallery-thumbs/09.jpg" class="img-responsive" alt="ditinggalrabi-wedding-photo-03">
+                                    </div>
+                                </a>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <a class="left carousel-control" href="#wedding-photo" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left"></span>
+                      </a>
+                      <a class="right carousel-control" href="#wedding-photo" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right"></span>
+                      </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- end:photos -->
+
+
     <!-- begin:copyright -->
     <section id="copyright">
             <div class="row">
@@ -166,8 +344,8 @@
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
-     <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false&amp;language=en"></script>
-    <script src="js/gmap3.js"></script>
+   {{--   <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false&amp;language=en"></script> --}}
+ {{-- <script src="js/gmap3.js"></script> --}}
     <script src="js/jquery.colorbox.js"></script>
     <script src="js/jquery.easing.js"></script> 
     <script src="js/script.js"></script>
